e8d98d31df17a4120621b3d112c0285eee7cd3c1
Merhene <merwan.st@gmail.com>
2024-05-30 10:16:05 +0200

    QUEST-06: done, results are ok

---
+++ src/Wilder.java
@@ -0,0 +1,34 @@
+public class Wilder {
+    private String firstname;
+    private boolean aware;
+
+    public Wilder(String firstname, boolean aware) {
+        this.firstname = firstname;
+        this.aware = aware;
+    }
+
+    public Wilder(boolean aware) {
+        this.aware = aware;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public boolean isAware() {
+        return aware;
+    }
+
+    public void setAware(boolean aware) {
+        this.aware = aware;
+    }
+
+    public String whoAmI() {
+        String awareMessage = this.aware ? "je suis aware" : "je suis pas aware";
+        return "Je m'appel " + this.firstname + " et " + awareMessage;
+    }
+}
